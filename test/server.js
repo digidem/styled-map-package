096b7804364a8d042ab42558ad9053e229427763
Gregor MacLennan <gmaclennan@digital-democracy.org>
2024-10-09 18:11:43 +0100

    feat(server): handle file replacement after server initialization (#25)

--- test/server.js
+++ test/server.js
@@ -1,19 +1,23 @@
 import { execa } from 'execa'
 import createFastify from 'fastify'
+import { temporaryFile, temporaryWrite } from 'tempy'
 
 import assert from 'node:assert/strict'
+import { randomBytes } from 'node:crypto'
+import fsPromises from 'node:fs/promises'
 import { test } from 'node:test'
 
 import createServer from '../lib/server.js'
 import { validateStyle } from '../lib/utils/style.js'
 import { replaceVariables } from '../lib/utils/templates.js'
 
-test('server basic', async () => {
+test('server basic', async (t) => {
   const filepath = new URL('./fixtures/demotiles-z2.smp', import.meta.url)
     .pathname
   const fastify = createFastify()
   fastify.register(createServer, { filepath })
   await fastify.listen()
+  t.after(() => fastify.close())
   const response = await fastify.inject({ url: '/style.json' })
   assert.equal(response.statusCode, 200)
   assert.equal(
@@ -68,7 +72,6 @@ test('server basic', async () => {
       'Content-Length header is correct',
     )
   }
-  await fastify.close()
 })
 
 test('server.close() closes reader', { skip: isWin() }, async () => {
@@ -95,24 +98,78 @@ test('server lazy', { skip: isWin() }, async () => {
   assert.equal(await fdCount(filepath), 0)
 })
 
-test('server invalid filepath', async () => {
+test('server invalid filepath', async (t) => {
   const filepath = 'invalid_file_path'
   const fastify = createFastify()
   fastify.register(createServer, { filepath })
   await fastify.listen()
+  t.after(() => fastify.close())
   const response = await fastify.inject({ url: '/style.json' })
-  assert.equal(response.statusCode, 500)
-  await fastify.close()
+  assert.equal(response.statusCode, 404)
 })
 
-test('server invalid file', async () => {
-  const filepath = '/dev/null'
+test('server invalid file', async (t) => {
+  const filepath = await temporaryWrite(randomBytes(1024))
   const fastify = createFastify()
   fastify.register(createServer, { filepath })
   await fastify.listen()
+  t.after(() => fastify.close())
   const response = await fastify.inject({ url: '/style.json' })
   assert.equal(response.statusCode, 500)
-  await fastify.close()
+})
+
+test('server file present after server starts', async (t) => {
+  const filepath = temporaryFile()
+  const smpFixtureFilepath = new URL(
+    './fixtures/demotiles-z2.smp',
+    import.meta.url,
+  ).pathname
+
+  const fastify = createFastify()
+  fastify.register(createServer, { filepath })
+  await fastify.listen()
+  t.after(() => fastify.close())
+
+  await t.test('file is not present initially', async () => {
+    const response = await fastify.inject({ url: '/style.json' })
+    assert.equal(response.statusCode, 404)
+    assert.match(response.json().error, /Not Found/)
+  })
+
+  await t.test('file is added after server starts', async () => {
+    await fsPromises.copyFile(smpFixtureFilepath, filepath)
+
+    const response = await fastify.inject({ url: '/style.json' })
+    assert.equal(response.statusCode, 200)
+    assert(validateStyle(response.json()))
+  })
+})
+
+test('invalid file replaced after server starts', async (t) => {
+  const filepath = await temporaryWrite(randomBytes(1024))
+  const smpFixtureFilepath = new URL(
+    './fixtures/demotiles-z2.smp',
+    import.meta.url,
+  ).pathname
+
+  const fastify = createFastify()
+  fastify.register(createServer, { filepath })
+  await fastify.listen()
+  t.after(() => fastify.close())
+
+  await t.test('file is not present initially', async () => {
+    const response = await fastify.inject({ url: '/style.json' })
+    assert.equal(response.statusCode, 500)
+    assert.match(response.json().error, /Internal Server Error/)
+  })
+
+  await t.test('file is added after server starts', async () => {
+    await fsPromises.copyFile(smpFixtureFilepath, filepath)
+
+    const response = await fastify.inject({ url: '/style.json' })
+    assert.equal(response.statusCode, 200)
+    assert(validateStyle(response.json()))
+  })
 })
 
 /** @returns {boolean} */
