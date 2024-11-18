18afee4c79dad7224b83e1c374840b2efc21f0ea
Gregor MacLennan <gmaclennan@digital-democracy.org>
2024-11-18 19:34:52 +0200

    feat: convert mbtiles to a styled map package file (#35)
    * feat: convert mbtiles to a styled map package file
    
    * update package-lock
    
    * address PR comments

---
+++ bin/smp-mbtiles.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+import { Command } from 'commander'
+import { pipeline } from 'stream/promises'
+
+import fromMBTiles from '../lib/from-mbtiles.js'
+
+const program = new Command()
+
+program
+  .description('Convert a MBTiles file to a styled map package file')
+  .option('-o, --output <file>', 'output smp file')
+  .argument('<mbtiles>', 'MBTiles file to convert')
+  .action(async (mbtilesPath, { output }) => {
+    if (output) {
+      await fromMBTiles(mbtilesPath, output)
+    } else {
+      await pipeline(fromMBTiles(mbtilesPath), process.stdout)
+    }
+  })
+
+program.parseAsync(process.argv)
---
+++ test/from-mbtiles.js
@@ -0,0 +1,61 @@
+import { MBTiles } from 'mbtiles-reader'
+import { temporaryFile } from 'tempy'
+
+import assert from 'node:assert/strict'
+import fs from 'node:fs'
+import { buffer } from 'node:stream/consumers'
+import { pipeline } from 'node:stream/promises'
+import test from 'node:test'
+import { fileURLToPath } from 'node:url'
+
+import convert from '../lib/from-mbtiles.js'
+import SMPReader from '../lib/reader.js'
+
+test('convert from MBTiles', async (t) => {
+  const fixture = fileURLToPath(
+    new URL('./fixtures/plain_1.mbtiles', import.meta.url),
+  )
+  const output1 = temporaryFile()
+  const output2 = temporaryFile()
+
+  t.after(() => {
+    fs.unlinkSync(output1)
+    fs.unlinkSync(output2)
+  })
+
+  await Promise.all([
+    convert(fixture, output1),
+    pipeline(convert(fixture), fs.createWriteStream(output2)),
+  ])
+
+  for (const output of [output1, output2]) {
+    const smp = new SMPReader(output)
+    const mbtiles = new MBTiles(fixture)
+    const style = await smp.getStyle('')
+    const sourceMetadata = Object.values(style.sources)[0]
+    assert.equal(sourceMetadata.type, 'raster')
+    let tileCount = 0
+    for (const { x, y, z, data } of mbtiles) {
+      tileCount++
+      const path = replaceVariables(sourceMetadata.tiles[0], { x, y, z })
+      const smpTile = await smp.getResource(path)
+      assert.deepEqual(await buffer(smpTile.stream), data)
+      assert.equal(smpTile.contentType, 'image/png')
+    }
+    assert(tileCount > 10, 'expected more than 10 tiles')
+  }
+})
+
+/**
+ * Replaces variables in a string with values provided in an object. Variables
+ * in the string are denoted by curly braces, e.g., {variableName}.
+ *
+ * @param {string} template - The string containing variables wrapped in curly braces.
+ * @param {Record<string, string | number>} variables - An object where the keys correspond to variable names and values correspond to the replacement values.
+ * @returns {string} The string with the variables replaced by their corresponding values.
+ */
+function replaceVariables(template, variables) {
+  return template.replace(/{(.*?)}/g, (match, varName) => {
+    return varName in variables ? String(variables[varName]) : match
+  })
+}
